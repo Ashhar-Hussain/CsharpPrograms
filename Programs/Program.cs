8b048f031255aed572be3cbdaa7caf40f11c7533
Ashhar-Hussain <ashhar.raeisi@gmail.com>
2022-07-22 18:49:29 +0500

    Programs

---
+++ Programs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int name;
+            name =Convert.ToInt32(Console.ReadLine());
+            if (name<=1)
+            {
+                Console.WriteLine(name);
+            }
+            else
+            {
+                Console.WriteLine("You are not");
+            }
+            Console.ReadLine();
+        }
+    }
+}
