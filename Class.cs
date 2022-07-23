ea97dbba3986e68dc2c3cee66a87bc1356c2cd19
Ashhar-Hussain <ashhar.raeisi@gmail.com>
2022-07-23 18:06:59 +0500

    go

--- Class.cs
+++ Class.cs
@@ -23,3 +23,25 @@ namespace Class
 
     }
 }
+//============================================================================================
+//Program.cs File
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class STudent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        public void Take_Exam(STudent abc)
+        {
+            Console.WriteLine("this student can take exam #" + abc.Name);
+        }
+    }
+}
