1db7769e537f2b26871c4f1eab30aba9bd174d6a
Ashhar-Hussain <ashhar.raeisi@gmail.com>
2022-07-23 18:08:08 +0500

    =>

---
+++ Class.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Program
+    {
+        static void Main()
+        {
+            STudent sTudent = new STudent();
+            sTudent.Id = 1;
+            sTudent.Name = "sTudent";
+            sTudent.Take_Exam(sTudent);
+
+            Console.WriteLine("The name is "+sTudent.Name+ "and its ID is" +sTudent.Id);
+            Console.ReadLine();
+        }
+        
+
+
+    }
+}
+//============================================================================================
+//Program.cs File
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class STudent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        public void Take_Exam(STudent abc)
+        {
+            Console.WriteLine("this student can take exam #" + abc.Name);
+        }
+    }
+}
---
+++ If-else.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Age;
+            Console.WriteLine("please enter your age");
+            Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is "+Age);
+            Console.ReadLine();
+        }
+    }
+}
