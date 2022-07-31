34618d9d5534be4f001fd3d9e99f917ce0524bf5
Ashhar-Hussain <ashhar.raeisi@gmail.com>
2022-07-31 14:15:21 +0500

    Create Calculator.cs

---
+++ Calculator.cs
@@ -0,0 +1,39 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+
+            int sum = a + b;
+            textBox3.Text = sum.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+
+            int sub = a - b;
+            textBox3.Text = sub.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+
+            int mul = a * b;
+            textBox3.Text = mul.ToString();
+        }
+    }
+}
